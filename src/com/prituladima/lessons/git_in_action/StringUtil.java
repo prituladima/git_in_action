db15cc1c5be6cb5f639e2cc6fa01a9f72f303638
dmprytul <z6ghdi4ogo75ojnesjqicnfhn5wz2v6xvo42lmq6scttwiqntxhq>
2021-03-31 20:39:55 +0300

    repeatAndJoin

--- src/com/prituladima/lessons/git_in_action/StringUtil.java
+++ src/com/prituladima/lessons/git_in_action/StringUtil.java
@@ -2,8 +2,19 @@ package com.prituladima.lessons.git_in_action;
 
 public class StringUtil {
 
-    public String repeatAndJoin(String val, int cnt, String separator){
-        return "";
+    public static void main(String[] args) {
+        System.out.println(repeatAndJoin("hello", 10, ", "));
+    }
+
+    public static String repeatAndJoin(String val, int cnt, String separator) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < cnt; i++) {
+            stringBuilder.append(val);
+            if (i < cnt - 1) {
+                stringBuilder.append(separator);
+            }
+        }
+        return stringBuilder.toString();
     }
 
     public char mostRepeatingChar(String val){
