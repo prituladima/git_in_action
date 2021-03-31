9b2e69df7cbaf27a0be4789560fa82033cda0714
JNT-PC\JNT <Jannet>
2021-03-31 20:48:58 +0300

    new com

--- src/com/prituladima/lessons/git_in_action/StringUtil.java
+++ src/com/prituladima/lessons/git_in_action/StringUtil.java
@@ -1,5 +1,8 @@
 package com.prituladima.lessons.git_in_action;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringUtil {
 
     public String repeatAndJoin(String val, int cnt, String separator){
@@ -7,7 +10,19 @@ public class StringUtil {
     }
 
     public char mostRepeatingChar(String val){
-        return '\0';
+        final Map <Character, Integer> charCnt = new HashMap<>();
+       char mostRepChar = '\0';
+       int maxCnt = 0;
+        for(int i = 0 ; i < val.length(); i++) {
+            char curChar = val.charAt(i);
+            charCnt.merge(curChar, 1, Integer::sum);
+            if(maxCnt < charCnt.get(curChar)){
+                maxCnt = charCnt.get(curChar);
+                mostRepChar = curChar;
+            }
+        }
+
+        return mostRepChar;
     }
 
 }
